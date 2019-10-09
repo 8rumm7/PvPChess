dfce7441941e6cdac50fe4a05c47d60d9a529ede
8rumm7 <marcus.daunert@stud.th-luebeck.de>
2019-10-09 16:27:12 +0200

    add stuff

--- src/schach/Main.java
+++ src/schach/Main.java
@@ -3,6 +3,9 @@ package schach;
 public class Main {
     public static void main(String[] args) {
 
+        Model model = new Model();
+        View view = new View(model);
+        Controller controller = new Controller(model, view);
 
     }
 }
