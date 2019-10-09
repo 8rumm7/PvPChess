415598e220eec91a5dd3f1e2b2bae006fbe004bf
8rumm7 <marcus.daunert@stud.th-luebeck.de>
2019-10-09 15:45:00 +0200

    innitial Commit

--- src/schach/Main.java
+++ src/schach/Main.java
@@ -2,8 +2,6 @@ package schach;
 
 public class Main {
     public static void main(String[] args) {
-        Model model = new Model();
-        View view = new View(model);
-        Controller controller = new Controller(model, view);
+        System.out.println("Hello world");
     }
 }
